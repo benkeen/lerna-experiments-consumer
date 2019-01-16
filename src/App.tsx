623e9b3d5bfba20e23d7d046459a1833a69fb186
Ben Keen <Ben.Keen@globalrelay.net>
2019-01-16 14:46:06 -0800

    cool. Now importing TS component with typings

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Button from '@benkeentestorg/buttons';
+
+
+class App extends Component {
+    render () {
+        return (
+            <div>
+                <p>
+                    Testing.
+                </p>
+
+                <Button label="Cool" />
+            </div>
+        );
+    }
+}
+
+export default App;
